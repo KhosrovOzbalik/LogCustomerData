5c9a12ca80efdbcee5f03781c0dfa1964a0a5456
maroofMaroof <98277463+maroofMaroof@users.noreply.github.com>
2022-01-23 21:44:30 +0300

    Creating the template
    Template created and basic classes created with it.

---
+++ src/Customer.java
@@ -0,0 +1,66 @@
+import java.util.regex.*;
+
+public class Customer {
+    //attributes
+    protected int id;
+    protected String name = "";
+    protected String surname = "";
+    protected String email;
+    protected String phoneNumber;
+    protected RealAddress address;
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean setEmail(String email) {
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            this.email = email;
+        }
+        return matcher.matches();
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public RealAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(RealAddress address) {
+        this.address = address;
+    }
+}
